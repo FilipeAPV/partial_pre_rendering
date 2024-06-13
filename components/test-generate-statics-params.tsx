04cdba11ddd09a4fdb2b71af8f5cac4f4ad54877
Filipe Vieira <filipe.vieira@scott-sports.com>
2024-06-13 13:42:46 +0200

    prod fix

--- components/test-generate-statics-params.tsx
+++ components/test-generate-statics-params.tsx
@@ -7,6 +7,11 @@ type TestGenerateStaticParamsProps = {
 export default async function TestGenerateStaticParams({
   language,
 }: TestGenerateStaticParamsProps) {
+  const isProd = process.env.NODE_ENV === 'production';
+  const domain = isProd
+    ? 'https://app-router-api.vercel.app'
+    : 'http://localhost:3000';
+
   const product = await fetch(`http://localhost:3000/api/cnb/${language}`);
   const data = await product.json();
   const message = data.message;
